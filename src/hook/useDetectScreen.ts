bb0c12702c9c272932863a2ccb602d636e7a6e1b
Truyen Nguyen <truyen.n@positivethinking.tech>
2022-03-01 23:20:40 +0700

    add constants file

---
+++ src/hook/useDetectScreen.ts
@@ -0,0 +1,7 @@
+import { MOBILE_WIDTH } from "constants/index";
+import useWindowSize from "hook/useWindowSize";
+const useDetectScreen = () => {
+  const [width] = useWindowSize();
+  return width <= MOBILE_WIDTH;
+};
+export default useDetectScreen;
