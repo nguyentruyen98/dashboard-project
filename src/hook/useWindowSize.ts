66e3dd2a6603486fe557d040d4d86e2184103c7a
Truyen Nguyen <truyen.n@positivethinking.tech>
2022-03-01 23:06:13 +0700

    add custome hook

---
+++ src/hook/useWindowSize.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  useEffect(() => {
+    const handleWindowResize = () =>
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  return [size.width, size.height];
+};
+export default useWindowSize;
--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,13 +1,19 @@
 import { Button as AntdButton } from "antd";
 import Buttom from "components/button";
+import useWindowResize from "hook/useWindowSize";
 import React from "react";
 const Home = () => {
+  const [width, height] = useWindowResize();
+
   return (
+    // drop every test case here
     <div>
       <Buttom type="dashed" danger>
         MY BUTTON
       </Buttom>
       <AntdButton>MY BUTTON</AntdButton>
+      <p>{`Current width: ${width}`}</p>
+      <p>{`Current height: ${height}`}</p>
     </div>
   );
 };
