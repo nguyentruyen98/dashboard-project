24ece1643aed4cc85ec08113480deacbe2c42c8d
Phantom(NA) <nguyennhatanh96@gmail.com>
2022-03-04 21:07:07 +0700

    feat: upgrade config source

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,20 +1,21 @@
-import "./index.scss";
+import './index.scss';
 
-import { Layout as AntdLayout } from "antd";
-import clsx from "clsx";
-import Header from "pages/layout/header";
-import Menu from "pages/layout/menu";
-import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import {Layout as AntdLayout} from 'antd';
+import clsx from 'clsx';
+import Header from 'pages/layout/header';
+import Menu from 'pages/layout/menu';
+import React, {useState} from 'react';
 
-const { Header: AntdHeader, Sider, Content } = AntdLayout;
+import {Outlet} from 'react-router-dom';
+
+const {Header: AntdHeader, Sider, Content} = AntdLayout;
 
 const Layout = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <AntdLayout className="layout">
-      <Sider className={clsx("sider", toggle && "sider-collapse")} width={250}>
+      <Sider className={clsx('sider', toggle && 'sider-collapse')} width={250}>
         <Menu toggle={toggle} setToggle={setToggle} />
       </Sider>
       <AntdLayout>
